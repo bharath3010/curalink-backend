62371baeb3ef8c342f8333728440c4e38502c01e
bharath3010 <143984799+bharath3010@users.noreply.github.com>
2025-12-12 08:48:43 +0000

    Add /meta/specialties endpoint

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import prisma from '../prisma.js';
-import requireAuth from '../middleware/auth.js';
+import requireAuth from '../middlewares/auth.js';
 
 const router = express.Router();
 
